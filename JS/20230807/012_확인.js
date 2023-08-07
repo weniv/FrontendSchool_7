13605aa52f7a05959fbf81a05c858a695aa11a55
paullabkorea <paullabkorea@gmail.com>
2023-08-07 18:15:07 +0900

    0807

---
+++ JS/20230807/012_확인.js
@@ -0,0 +1,20 @@
+let value = 0
+[10, 20, 30].forEach(v =>
+    console.log(v)
+)
+
+let value = [10, 20, 30]
+let value2 = value
+[0, 1, 2]
+// value2 = value[2]
+
+0[10, 20, 30] // 자동세미콜론 삽입 X (Automatic Semicolon Insertion, ASI) 기능
+
+10, 20, 30 // 30으로 평가
+0[30] // 앞에 0을 object로 평가
+0[30] // 이러한 값이 없기 때문에 undefined로 평가
+// undefined.forEach()
+
+// 2개월
+// 6 ~ 20명
+// 200page
